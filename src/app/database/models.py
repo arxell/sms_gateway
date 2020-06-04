1bb8ea30897e1567c5a94db3fe0eea37af8b7de6
Anton Ogorodnikov <a.ogorodnikov@softpro.com>
2020-06-04 18:00:29 +0300

    fix

--- src/app/database/models.py
+++ src/app/database/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Enum, Integer, String
+import datetime as dt
+
+from sqlalchemy import Column, DateTime, Enum, Integer, String
 
 from app.constants import SmsProvider
 
@@ -9,8 +11,9 @@ class SmsMessage(ModelBase):
     __tablename__ = "sms_message"
 
     id = Column(Integer, primary_key=True, index=True)
-    provider_name = Column(Enum(SmsProvider))
-    provider_message_id = Column(String)
+    provider_name = Column(Enum(SmsProvider), nullable=False, create_type=False)
+    provider_message_id = Column(String, nullable=False)
+    created_at = Column(DateTime, default=dt.datetime.utcnow, nullable=False)
 
     def __str__(self) -> str:
         return f'{self.id} {self.provider_name} {self.provider_message_id}'
